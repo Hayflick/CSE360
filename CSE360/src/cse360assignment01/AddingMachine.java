e7d73fcafd50535450c68bb162ab3c3e61b149ba
Hayflick <Hayflick@users.noreply.github.com>
2020-10-04 20:11:05 -0700

    Change 2 - 10/4/2020
    Declared additional class variable ArrayList to record operation
    history, defined methods, and updated documentation accordingly.

--- CSE360/src/cse360assignment01/AddingMachine.java
+++ CSE360/src/cse360assignment01/AddingMachine.java
@@ -1,5 +1,9 @@
 package cse360assignment01;
 
+import java.io.*;
+import java.util.*;
+
+
 /*
  *The AddingMachine program implements an application that allows for basic arithmetic 
  *operations such as addition and attraction using integer values, as well as the
@@ -14,31 +18,70 @@ package cse360assignment01;
 public class AddingMachine {
 	
   private int total;	//	variable returned to user containing total of the operations performed
+  private ArrayList<String> history = new ArrayList<String>();	//	list that records all operations
   
-  //Constructor that initializes class variable total to 0
+  //Constructor that initializes class variable total to 0 and adds a 0 to the beginning of history
   public AddingMachine () {
-    total = 0;  
+	  
+    total = 0;
+    history.add("0");
+  
   }
   
   //Function that returns the total class variable
   public int getTotal () {
-    return 0;
+	  
+    return total;
+  
   }
   
-  //Function that adds integer values to the total class variable
+  /*
+   *Function that adds integer values to the total class variable and makes a record of the
+   *operation in the history ArrayList
+  */
   public void add (int value) {
+	  
+	  total = total + value;
+	  
+	  history.add("+");
+	  history.add(Integer.toString(value));
+  
   }
 
-  //Function that subtracts integer values from the total class variable
+  /*
+   * Function that subtracts integer values from the total class variable and makes a record of the
+   * operation in the history ArrayList
+  */
   public void subtract (int value) {
+  
+	  total = total - value;
+	  
+	  history.add("-");
+	  history.add(Integer.toString(value));
   }
 
-  //Function returning string representation of the total result
+  //Function returning string representation of the history of operations performed via ArrayList
   public String toString () {
-    return "";
+	  
+	  String summary = "";
+	  
+	  for(int i = 0; i < history.size(); i++)
+	  {
+		  summary = summary + (history.get(i) + " ");
+	  }
+	  
+	  return summary;
+  
   }
 
-  //Function resetting total class variable to 0
+  /*
+   * Function clearing memory of all previous operations from the ArrayList and resetting the total
+   * value to 0.
+   * */
   public void clear() {
+	  
+	  total = 0;
+	  history.clear();
+	  
   }
 }
