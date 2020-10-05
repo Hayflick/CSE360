5a0dee08e5f181c088d8ad37ed87c23b72e2f195
Aria Salehi <Hayflick@users.noreply.github.com>
2020-10-04 19:05:29 -0700

    Initial submission of AddingMachine prior to making code changes.

---
+++ CSE360/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assignment01;
+
+/*
+ *The AddingMachine program implements an application that allows for basic arithmetic 
+ *operations such as addition and attraction using integer values, as well as the
+ *returning of the result of these operations and a method to display the result
+ *via standard output.
+ *
+ * @author Aria Salehi
+ * @version 1.0
+ * @since 10/4/2020
+ */
+
+public class AddingMachine {
+	
+  private int total;	//	variable returned to user containing total of the operations performed
+  
+  //Constructor that initializes class variable total to 0
+  public AddingMachine () {
+    total = 0;  
+  }
+  
+  //Function that returns the total class variable
+  public int getTotal () {
+    return 0;
+  }
+  
+  //Function that adds integer values to the total class variable
+  public void add (int value) {
+  }
+
+  //Function that subtracts integer values from the total class variable
+  public void subtract (int value) {
+  }
+
+  //Function returning string representation of the total result
+  public String toString () {
+    return "";
+  }
+
+  //Function resetting total class variable to 0
+  public void clear() {
+  }
+}
